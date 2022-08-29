d23effd9f79d42ef2ddcdacba35d0c7648314c45
Rioba-Ian-Felix <riobafelix@gmail.com>
2022-08-29 21:37:56 +0300

    "added frontend"

--- fastApiReactApp/frontend/src/App.jsx
+++ fastApiReactApp/frontend/src/App.jsx
@@ -1,34 +1,91 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState, useEffect, useRef } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [clientId, setClienId] = useState(
+    Math.floor(new Date().getTime() / 1000)
+  );
+
+  const [chatHistory, setChatHistory] = useState([]);
+  const [isOnline, setIsOnline] = useState(false);
+  const [textValue, setTextValue] = useState("");
+  const [websckt, setWebsckt] = useState();
+
+  const [message, setMessage] = useState([]);
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const url = "ws://https:8000/ws/" + clientId;
+    const ws = new WebSocket(url);
+
+    ws.onopen = (event) => {
+      ws.send("Connect");
+    };
+
+    // recieve message every start page
+    ws.onmessage = (e) => {
+      const message = JSON.parse(e.data);
+      setMessages([...messages, message]);
+    };
+
+    setWebsckt(ws);
+    //clean up function when we close page
+    return () => ws.close();
+  }, [message, messages]);
+
+  const sendMessage = () => {
+    websckt.send(message);
+    // recieve message every send message
+    websckt.onmessage = (e) => {
+      const message = JSON.parse(e.data);
+      setMessages([...messages, message]);
+    };
+    setMessage([]);
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <h1>Chat</h1>
+      <h2>Your client id: {clientId} </h2>
+      <div className="chat-container">
+        <div className="chat">
+          {messages.map((value, index) => {
+            if (value.clientId === clientId) {
+              return (
+                <div key={index} className="my-message-container">
+                  <div className="my-message">
+                    <p className="client">client id : {clientId}</p>
+                    <p className="message">{value.message}</p>
+                  </div>
+                </div>
+              );
+            } else {
+              return (
+                <div key={index} className="another-message-container">
+                  <div className="another-message">
+                    <p className="client">client id : {clientId}</p>
+                    <p className="message">{value.message}</p>
+                  </div>
+                </div>
+              );
+            }
+          })}
+        </div>
+        <div className="input-chat-container">
+          <input
+            className="input-chat"
+            type="text"
+            placeholder="Chat message ..."
+            onChange={(e) => setMessage(e.target.value)}
+            value={message}
+          ></input>
+          <button className="submit-chat" onClick={sendMessage}>
+            Send
+          </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
